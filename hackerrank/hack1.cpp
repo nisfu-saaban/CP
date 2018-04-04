83e0dd2f7fd1667182f07f8ee92f89bdb0238b33
nisfu-saaban <nisthy07@gmail.com>
2018-04-04 14:45:44 +0700

    status accept

---
+++ hackerrank/hack1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int a;
+int b;
+int c=0;
+cin>>a;
+for(int i=0;i<a;i++){
+        cin>>b;
+        c+=b;
+    }
+    cout<<endl;
+    cout<<c;
+}
